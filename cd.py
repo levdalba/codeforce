274f19b29f4e30fee1a9e50918123659ba0915e9
levdalba <levanidalbashvili00@gmail.com>
2023-11-05 03:15:00 +0700

    Cama 2023 1

---
+++ cd.py
@@ -0,0 +1,7 @@
+s = str(input())
+if s == "Yoda":
+    print("Pertenece a Star Wars.")
+elif s == "Spock":
+    print("Pertenece a Star Trek.")
+elif s == "Frodo":
+    print("No pertenece ni a Star Wars ni a Star Trek.")
