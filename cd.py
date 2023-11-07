b67c666e891ca5da5d29f895b4a7bc8b3a221674
levdalba <levanidalbashvili00@gmail.com>
2023-11-07 22:47:10 +0700

    4 group

--- cd.py
+++ cd.py
@@ -1,10 +1,18 @@
-s = input().strip()
-open = close = count = 0
+H, W = map(int, input().split())
+N = int(input())
+A = list(map(int, input().split()))
 
-for i in range(len(s) - 1, -1, -1):
-    if s[i : i + 2] == "))":
-        close += 1
-    elif s[i : i + 2] == "((":
-        count += close
+colors = [i + 1 for i in range(N) for _ in range(A[i])]
 
-print(count)
+table = [[0] * W for _ in range(H)]
+i = 0
+for r in range(H):
+    for c in range(W):
+        if r % 2 == 0:
+            table[r][c] = colors[i]
+        else:
+            table[r][W - c - 1] = colors[i]
+        i += 1
+
+for row in table:
+    print(*row)
