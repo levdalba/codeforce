dd8009911d04b78734edec1b6be8e59fcc8cdb11
levdalba <levanidalbashvili00@gmail.com>
2023-11-07 21:57:23 +0700

    group 2

--- cd.py
+++ cd.py
@@ -1,10 +1,17 @@
-n = int(input())
-ans = []
-while n > 1:
-    x = n // 2
-    ans.append(x)
-    n -= x
+n = int(input().strip())
+count = 0
 
-ans.reverse()
-print(len(ans))
-print(*ans)
+for _ in range(n):
+    s = input().strip()
+    stack = []
+
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+
+    if len(stack) == 0:
+        count += 1
+
+print(count)
