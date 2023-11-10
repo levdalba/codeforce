387dfee47b4b52eac4422448c97b7593bf5bf9aa
levdalba <levanidalbashvili00@gmail.com>
2023-11-11 01:27:40 +0700

    number spiral

--- cd.py
+++ cd.py
@@ -1,14 +1,13 @@
-n = int(input())
-list = []
-if n == 1:
-    print(1)
-if n > 3:
-    for i in range(1, n + 1):
-        if i % 2 == 0:
-            list.append(i)
-    for j in range(1, n + 1):
-        if j % 2 != 0:
-            list.append(j)
-    print(" ".join(map(str, list)))
-else:
-    print("NO SOLUTION")
+def number_spiral(y, x):
+    z = max(y, x)
+    d = z * (z - 1) + 1
+    if z % 2 == 0:
+        return d + y - x
+    else:
+        return d - y + x
+
+
+t = int(input())
+for _ in range(t):
+    y, x = map(int, input().split())
+    print(number_spiral(y, x))
