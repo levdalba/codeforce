524e5d23db55d5df3ab89d911964a26c61940a28
levdalba <levanidalbashvili00@gmail.com>
2023-11-10 13:34:10 +0700

    cses Increasing Array

--- cd.py
+++ cd.py
@@ -1,18 +1,10 @@
-H, W = map(int, input().split())
-N = int(input())
-A = list(map(int, input().split()))
+n = int(input())
+nums = list(map(int, input().split()))
 
-colors = [i + 1 for i in range(N) for _ in range(A[i])]
+moves = 0
+for i in range(1, n):
+    if nums[i] < nums[i - 1]:
+        moves += nums[i - 1] - nums[i]
+        nums[i] = nums[i - 1]
 
-table = [[0] * W for _ in range(H)]
-i = 0
-for r in range(H):
-    for c in range(W):
-        if r % 2 == 0:
-            table[r][c] = colors[i]
-        else:
-            table[r][W - c - 1] = colors[i]
-        i += 1
-
-for row in table:
-    print(*row)
+print(moves)
