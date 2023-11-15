584e71adf5e811589caccb3f0050f3cba397eec9
levdalba <levanidalbashvili00@gmail.com>
2023-11-16 00:31:53 +0700

    the pleasant walk

--- cd.py
+++ cd.py
@@ -1,6 +1,16 @@
-n = int(input())
-for k in range(1, n + 1):
-    total_positions = k * k
-    total_ways = total_positions * (total_positions - 1) // 2
-    attacking_ways = 4 * (k - 1) * (k - 2)
-    print(total_ways - attacking_ways)
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+max_length = 0
+current_length = 1
+
+for i in range(1, n):
+    if a[i] != a[i - 1]:
+        current_length += 1
+    else:
+        max_length = max(max_length, current_length)
+        current_length = 1
+
+max_length = max(max_length, current_length)
+
+print(max_length)
